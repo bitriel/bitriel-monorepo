9a439afc08bf67a553de5c7d3280199036fe3b00
Sam Allen <sokunsamnang45@gmail.com>
2025-11-14 15:57:42 +0700

    feat: update Vite logo asset and fix import path in App component

--- apps/mobile/components/__tests__/themed-text.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react-native';
-
-import { ThemedText } from '../themed-text';
-
-it(`renders correctly`, () => {
-  const component = render(<ThemedText>Snapshot test!</ThemedText>);
-  expect(component.toJSON()).toMatchSnapshot();
-});
